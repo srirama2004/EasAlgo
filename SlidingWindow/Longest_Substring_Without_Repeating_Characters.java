65eb0bdd9ea9ae54e0a57af8e9b82dc1098a852a
Srirama V <161791032+srirama2004@users.noreply.github.com>
2025-06-28 23:38:59 +0530

    Added/Updated Longest_Substring_Without_Repeating_Characters.java

--- SlidingWindow/Longest_Substring_Without_Repeating_Characters.java
+++ SlidingWindow/Longest_Substring_Without_Repeating_Characters.java
@@ -1,11 +1,7 @@
 
 /*
 Explanation:
-Given a string, S. Find the length of the longest substring without repeating characters. (Hard)
-Hint 1
-Use a hash map (or set) to store the characters currently in the window. For each character, If it's not in the hash map, add it to the window and update the maximum length. If it's already in the hash map, move the left pointer forward until the duplicate is removed.
-Hint 2
-After processing each character, compute the length of the current window as right−left+1. Keep track of the maximum length encountered during the traversal.
+Given a string, S. Find the length of the longest substring without repeating characters.
 */
 
 class Solution {
