5719d2e08294a8adde43586ff59f75fd015c0363
Srirama V <161791032+srirama2004@users.noreply.github.com>
2025-07-27 23:36:46 +0530

    Added/Updated Set_Matrix_Zeros.java

---
+++ G&L/Set_Matrix_Zeros.java
@@ -0,0 +1,30 @@
+
+/*
+Explanation:
+You are given a 2D matrix mat[][] of size n x m. The task is to modify the matrix such that if mat[i][j] is 0, all the elements in the i-th row and j-th column are set to 0.
+*/
+
+class Solution {
+    public void setMatrixZeroes(int[][] mat) {
+        ArrayList<Integer> row=new ArrayList<>();
+        ArrayList<Integer> col=new ArrayList<>();
+        for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(mat[i][j]==0){
+                    row.add(i);
+                    col.add(j);
+                }
+            }
+        }
+        for(int k=0;k<row.size();k++){
+           for(int i=0;i<mat.length;i++){
+            for(int j=0;j<mat[0].length;j++){
+                if(row.get(k)==i || col.get(k)==j ){
+                    mat[i][j]=0;
+                }
+                    
+                }
+            }
+        }        
+    }
+}
