6b06b4242f6995f0bf8d42c58350bf2483788398
Srirama V <161791032+srirama2004@users.noreply.github.com>
2025-08-01 23:13:06 +0530

    Added/Updated Balancing_Consonants_and_Vowels_Ratio.java

---
+++ G&L/Balancing_Consonants_and_Vowels_Ratio.java
@@ -0,0 +1,38 @@
+
+/*
+Explanation:
+You are given an array of strings arr[], where each arr[i] consists of lowercase english alphabets. You need to find the number of balanced strings in arr[] which can be formed by concatinating one or more contiguous strings of arr[].
+A balanced string contains the equal number of vowels and consonants. 
+*/
+
+class Solution {
+    boolean isvowel(char ch)
+    {
+        return (ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
+    }
+    public int countBalanced(String[] arr) {
+        // code here
+        int n=arr.length;
+        Map<Integer,Integer>mp=new HashMap<>();
+        mp.put(0,1);
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            int curr=0;
+            String str=arr[i];
+            for(int j=0;j<str.length();j++)
+            {
+                char ch=str.charAt(j);
+                if(isvowel(ch))curr++;
+                else curr--;
+            }
+            sum+=curr;
+            if(mp.containsKey(sum))ans+=mp.get(sum);
+            mp.put(sum,mp.getOrDefault(sum,0)+1);
+        }
+        return ans;
+    }
+}
+
+
